ababa309b21543d7c60506f00ab33690d189134a
Yannick Höffner <yannick.hoeffner@uni-jena.de>
2026-07-16 15:36:27 +0200

    added esp arduino-ide code

---
+++ esp_alarm_udp/esp_alarm_udp.ino
@@ -0,0 +1,78 @@
+
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include "secrets.h"
+
+
+IPAddress target_ip(192, 168, 2, 64); // IPv4 address of desktop computer running the alarm application
+unsigned int target_port = 64000; 
+char alarm_message[] = "alarm received\r\n";
+unsigned long last_send_time = 0;
+const unsigned long min_time_between_ms = 100; 
+
+WiFiUDP Udp;
+
+unsigned long last_alarm_received_time = 0;
+const unsigned long alarm_on_time_ms = 5000; // number of ms the alarm led should stay on after received an alarm
+
+bool alarm_received = false;
+
+const int button_pin = 4; // use GPIO 4 == D2
+const int relay_pin = 5; //use GPIO 5 == D1
+
+void setup() {   
+  // initialize inbuilt LED pin as an output.
+  pinMode(relay_pin, OUTPUT);
+  pinMode(button_pin, INPUT); 
+
+  
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(STASSID, STAPSK);
+  while (WiFi.status() != WL_CONNECTED) {
+    if (digitalRead(button_pin)){
+      // switch the relay
+      digitalWrite(relay_pin, HIGH);
+      // turn on timer for alarm reset
+      last_alarm_received_time = millis();
+      alarm_received = true;
+    }
+  
+    delay(50);
+  }
+  // if alarm received before boot, immediately send udp
+  if (alarm_received){
+    last_send_time = millis();
+    Udp.beginPacket(target_ip, target_port);
+    Udp.write(alarm_message);
+    Udp.endPacket();
+  }
+}
+
+// loop function runs over and over  again forever
+void loop() {
+  unsigned long current_millis = millis();
+
+  if (alarm_received){
+    if (current_millis - last_alarm_received_time >= alarm_on_time_ms) {
+      last_alarm_received_time = current_millis;
+      digitalWrite(relay_pin, LOW);    // turn the LED off by making the  pin 13 LOW
+      alarm_received = false;
+    }
+  }
+  
+  if (digitalRead(button_pin)){
+    // switch the relay
+    digitalWrite(relay_pin, HIGH);
+    // turn on timer for alarm reset
+    last_alarm_received_time = current_millis;
+    alarm_received = true;
+
+    if (current_millis - last_send_time >= min_time_between_ms){
+      last_send_time = current_millis;
+      Udp.beginPacket(target_ip, target_port);
+      Udp.write(alarm_message);
+      Udp.endPacket();
+    }
+  }
+}
---
+++ esp_alarm_udp/secrets.example.h
@@ -0,0 +1,4 @@
+#ifndef STASSID
+#define STASSID "my_wifi_ssid"
+#define STAPSK "my_wifi_pssw"
+#endif
